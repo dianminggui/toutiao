ef6ae1044a36c0a4e02a1fedacc68415bba8fb6f
金伟强 <iamjoel007@gmail.com>
2017-04-24 22:43:19 +0800

    adds

--- src/index/main.js
+++ src/index/main.js
@@ -2,17 +2,34 @@
   var app = new Vue({
     el: '#app',
     data: {
-      articles: [] // 文章 
+      types: [],
+      currType: '',
+      articles: [], // 文章
     },
     mounted() {
-      // 获取某类型下的所有文章
-      this.fetchArticles('fun')
+      var vm = this
+      this.fetchTypes().then(()=> {
+        if(vm.types.length > 0) {
+          vm.currType = vm.types[0].id
+          // 获取某类型下的所有文章
+          this.fetchArticles(vm.currType)
+        }
+      })
     },
     methods: {
       fetchArticles(typeId) {
-        axios.get(`/type/${typeId}`).then(response => {
-          console.log(response.data)
-        }) 
+        this.currType = typeId
+        return axios.get(`/type/${typeId}`).then(response => {
+          this.articles = response.data
+        })
+      },
+      fetchTypes() {
+        return axios.get(`/allTypes`).then(response => {
+          this.types = response.data
+        })
+      },
+      viewArticle(article) {
+        alert(`查看name为${article.title},id为${article.id}的文章`)
       }
     }
   })
--- src/index/mock.js
+++ src/index/mock.js
@@ -1,17 +1,66 @@
 ; {
+  // 所有类型
+  var allTypes = [{
+    name: '娱乐',
+    id: 'fun'
+  }, {
+    name: '游戏',
+    id: 'game'
+  }, {
+    name: '汽车',
+    id: 'car'
+  }]
+  Mock.mock(new RegExp('/allTypes'), options => {
+    return allTypes
+  })
+
   // 按类型分类的所有文章
-  var allArticles = {
-    'fun': [{ // 娱乐
-      id: 1,
-      title: '3岁无脸孩子的cosplay 吓坏昆凌孟非 卸妆后全场尖叫',
-      time: '10分钟前',
-      author: 'xxx'
-    }],
-    'game': [],
-    'other': [],
-  }
+  var allArticles = [{
+    id: 1,
+    title: '3岁无脸孩子的cosplay 吓坏昆凌孟非 卸妆后全场尖叫',
+    time: '10分钟前',
+    author: '头条娱乐',
+    type: 'fun'// 娱乐
+  }, {
+    id: 2,
+    title: '赵四刘小光睡粉让女孩刷礼物，2万不还直接拉黑！',
+    time: '32分钟前',
+    author: '星探光光',
+    type: 'fun' // 娱乐
+  },
+  {
+    id: 3,
+    title: '3岁无脸孩子的cosplay 吓坏昆凌孟非 卸妆后全场尖叫',
+    time: '1小时前',
+    author: '头条娱乐',
+    type: 'fun' // 娱乐
+  },
+  {
+    id: 4,
+    title: '猫娘游戏《NEKO-NIN exHeart》上架Steam 爱猫人士的新选择',
+    time: '10分钟前',
+    author: ' 游民星空 ⋅',
+    type: 'game'// 游戏
+  },
+  {
+    id: 5,
+    title: ' 央视直播失误集锦第三季！“一本正经地胡言乱语”是种怎样的体验',
+    time: '1小时前',
+    author: 'xxx',
+    type: 'game'// 游戏
+  },
+  {
+    id: 6,
+    title: ' A1、A2、A3、B1、B2驾驶证“降级”新规定(2017)',
+    time: '10分钟前',
+    author: '女司机谈车',
+    type: 'car'// 汽车
+  }]
   Mock.mock(new RegExp('/type'), options => {
     var typeId = /^\/type\/(.*)/.exec(options.url)[1]
-    return allArticles[typeId] || []
+    // debugger
+    return allArticles.filter(article => {
+      return typeId === article.type
+    })
   })
 }
